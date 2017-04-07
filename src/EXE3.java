681b6c17ae9a17750901a50fa7cd4d251caaa013
matheus.jmaia <matheus.jmaia@CAS555573W0720.senacsp.edu.br>
2017-04-06 22:08:49 -0300

    SOrteio

---
+++ src/EXE3.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+
+public class EXE3 {
+public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        
+    
+}
+}
---
+++ src/EXE7.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+
+public class EXE7 {
+
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Digite a idade do idade do conveniado");
+        int idade = console.nextInt();
+        if (idade < 10) {
+            int valor = 100 + 80;
+            System.out.println("O valor a ser pago será " + valor + " Reais.");
+        }
+        if ((idade >= 10) && (idade <= 30)) {
+            int valor = 100 + 50;
+            System.out.println("O valor a ser pago será " + valor + " Reais.");
+        }
+        if ((idade >= 40) && (idade <= 60)) {
+            int valor = 100 + 95;
+            System.out.println("O valor a ser pago será " + valor + " Reais.");
+    }
+        if ((idade >= 40) && (idade <= 60)) {
+            int valor = 100 + 95;
+            System.out.println("O valor a ser pago será " + valor + " Reais.");
+    }
+    }
+}
